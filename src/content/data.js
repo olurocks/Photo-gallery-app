2ea1a3e7aa5f55248f00bfe0988ae77ed736a526
olurocks <olurocks1@gmail.com>
2023-06-11 16:29:48 +0100

    initial commit

---
+++ src/content/data.js
@@ -0,0 +1,65 @@
+export const data = [
+  {
+    "name": "Photo 1",
+    "url": "/images/insta_1.png",
+    "description": "Photo 1 description",
+    "date": "2020-01-01",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 2",
+    "url": "/images/insta_2.png",
+    "description": "Photo 2 description",
+    "date": "2020-01-02",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 3",
+    "url": "/images/insta_3.png",
+    "description": "Photo 3 description",
+    "date": "2020-01-03",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 4",
+    "url": "/images/insta_4.png",
+    "description": "Photo 4 description",
+    "date": "2020-01-04",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 5",
+    "url": "/images/insta_5.png",
+    "description": "Photo 5 description",
+    "date": "2020-01-05",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 6",
+    "url": "/images/insta_6.png",
+    "description": "Photo 6 description",
+    "date": "2020-01-06",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 7",
+    "url": "/images/insta_7.png",
+    "description": "Photo 7 description",
+    "date": "2020-01-07",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 8",
+    "url": "/images/insta_8.png",
+    "description": "Photo 8 description",
+    "date": "2020-01-08",
+    "type": "image/png",
+  },
+  {
+    "name": "Photo 9",
+    "url": "/images/insta_9.png",
+    "description": "Photo 9 description",
+    "date": "2020-01-09",
+    "type": "image/png",
+  }
+]
